8abaadbb603754c4ebae1085fba47596d5a60316
harshita-2310-singh <harshitasingh2310@gmail.com>
2025-11-19 15:42:00 +0530

    sql

---
+++ sql.sql
@@ -0,0 +1,13 @@
+
+select * from employee;
+
+
+CREATE DATABASE studentdb;
+USE studentdb;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT
+);
+select * from students;
